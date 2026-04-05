eec849de9a85e2ebd88f00cec48ab438a587de0e
McCauley Armishaw <151598523+marmishaw14@users.noreply.github.com>
2026-04-05 16:12:33 -0500

    feat(controllers): add controller function for creating a weapon

---
+++ src/api/v1/controllers/weaponController.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import * as weaponService from "../services/weaponService";
+import { successResponse } from "../models/responseModel";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
+import { Weapon } from "../models/weaponModel";
+
+/**
+ * Handles creating a new weapon.
+ * @param {Request} req - The request object. 
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>} 
+ */
+export const createWeaponHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const weapon: Weapon = await weaponService.createWeapon(req.body);
+        res.status(HTTP_STATUS.CREATED).json(successResponse(weapon));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
