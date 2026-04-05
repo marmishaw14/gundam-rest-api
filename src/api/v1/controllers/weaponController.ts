ef7e81b52ae09c7f3b93a7d040a05167dd2a5918
McCauley Armishaw <151598523+marmishaw14@users.noreply.github.com>
2026-04-05 16:31:50 -0500

    feat(controllers): add controller function for getting all weapons

--- src/api/v1/controllers/weaponController.ts
+++ src/api/v1/controllers/weaponController.ts
@@ -19,3 +19,19 @@ export const createWeaponHandler = async (req: Request, res: Response, next: Nex
         next(error);
     }
 };
+
+/**
+ * Handles getting all weapons.
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>}
+ */
+export const getAllWeaponsHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const weapons: Weapon[] = await weaponService.getAllWeapons();
+        res.status(HTTP_STATUS.OK).json(successResponse(weapons, "Weapons available for use retrieved successfully."));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
