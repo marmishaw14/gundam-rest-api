f70ecbe903a8a9495ddf36c1fb4f3c5e282a1fe4
McCauley Armishaw <151598523+marmishaw14@users.noreply.github.com>
2026-04-14 10:31:57 -0500

    feat(routes): set up create pilot route

---
+++ src/api/v1/routes/pilotRoutes.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { validateRequest } from "../middleware/validate";
+import * as pilotController from "../controllers/pilotController";
+import { pilotSchemas } from "../validation/pilotSchemas";
+
+const pilotRouter = express.Router();
+
+pilotRouter.post(
+    "/pilots",
+    validateRequest(pilotSchemas.create),
+    pilotController.createPilotHandler
+);
+
+export default pilotRouter;
