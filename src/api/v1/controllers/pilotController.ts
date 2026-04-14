4baba42512b5bd089780147a787e8fc99e068e8c
McCauley Armishaw <151598523+marmishaw14@users.noreply.github.com>
2026-04-14 11:52:10 -0500

    feat(controllers): add controller functions for getting pilot by id, updating pilot by id, and deleting a pilot by id

--- src/api/v1/controllers/pilotController.ts
+++ src/api/v1/controllers/pilotController.ts
@@ -20,10 +20,65 @@ export const createPilotHandler = async (req: Request, res: Response, next: Next
     }
 };
 
+/**
+ * Handles getting all pilots.
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>}
+ */
 export const getAllPilotsHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         const pilots: Pilot[] = await pilotService.getAllPilots();
         res.status(HTTP_STATUS.OK).json(successResponse(pilots, "List of pilots retrieved successfully."));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+/**
+ * Handles getting a pilot by their id.
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>}
+ */
+export const getPilotByIdHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const pilot: Pilot = await pilotService.getPilotById(req.params.id as string);
+        res.status(HTTP_STATUS.OK).json(successResponse(pilot, `Pilot with id: ${req.params.id} retrieved successfully.`));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+/**
+ * Handles updating a pilot by their id.
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>}
+ */
+export const updatePilotByIdHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const pilot: Pilot = await pilotService.updatePilotById(req.params.id as string, req.body);
+        res.status(HTTP_STATUS.OK).json(successResponse(pilot, "Pilot information updated."));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+/**
+ * Handles deleting/removing a pilot by their id.
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>}
+ */
+export const deletePilotHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        await pilotService.deletePilotById(req.params.id as string);
+        res.status(HTTP_STATUS.OK).json(successResponse({}, "Pilot deleted."));
     } catch (error: unknown) {
         next(error);
     }
